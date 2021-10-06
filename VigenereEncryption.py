74f34d0474dc7d31914475cd89a505bc18f2c647
ValColorado <valentina_colorado@yahoo.com>
2021-10-06 09:05:52 -0400

    update accepts text file and sends decryption for cryptanalysis

--- VigenereEncryption.py
+++ VigenereEncryption.py
@@ -3,6 +3,7 @@ Project 1
 Encrypt & Decrypt Vigenere Cipher
 """
 import math
+from openText import open_text
 
 #asciiValues[29] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
 asciiLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '=', '+', '*']
@@ -22,12 +23,12 @@ def encrypt(plainT, keyS): #function takes plaintext letter and key letter
         for i in range(29): # gets ascii numeric for plaintext
             if asciiLetters[i] == plainT[letter]:
                 plain = i
-        print('plain: ' + str(plain))
+        #print('plain: ' + str(plain))
         
         for j in range(29): # gets ascii numeric for key
             if asciiLetters[j] == keyS[letter % len(keyS)]:
                 key = j
-        print('key: ' + str(key))
+        #print('key: ' + str(key))
 
         cipher = (plain + key) % 29
         
@@ -52,12 +53,12 @@ def decrypt(cipherT, keyS):
         for i in range(29): # gets ascii numeric for plaintext
             if asciiLetters[i] == cipherT[letter]:
                 cipher = i
-        print('cipher: ' + str(cipher))
+        #print('cipher: ' + str(cipher))
         
         for j in range(29): # gets ascii numeric for key
             if asciiLetters[j] == keyS[letter % len(keyS)]:
                 key = j
-        print('key: ' + str(key))
+        #print('key: ' + str(key))
 
         plain = (cipher - key) % 29
         
@@ -68,16 +69,21 @@ def decrypt(cipherT, keyS):
     
     
 
-def main():
-    plainText = 'carnitas'
+def mainV():
+    textFile = open_text()
+    plainText = textFile
+   # print(plainText)
     key = 'lee'
-    print('encrypt:')
+   # print('encrypt:')
     encryption = encrypt(plainText, key)
-    print('decrypt:')
+    #print('decrypt:')
     decryption = decrypt(encryption, key)
-    print('e = ' + encryption)
-    print('d = ' + decryption)
+  #  print('e = ' + encryption)
+   # print('d = ' + decryption)
+    print("sending over string for cryptanalysis..")
+    return decryption
+    
 
 
-main()
+mainV()
 
