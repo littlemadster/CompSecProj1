37798045e0c614591f1c53752e25e7ced476ab16
ValColorado <valentina_colorado@yahoo.com>
2021-10-10 13:29:20 -0400

    TESTER file for LanguageLetters

---
+++ TESTER-LanguageLetters.py
@@ -0,0 +1,14 @@
+"""
+Project 1
+Tester File: LanguageLetters.py
+By Maddie Sirok && Valentina Colorado
+
+"""
+from LanguageLetters import LanguageLetters
+
+
+def main():
+    #openFile(INPUT FILENAME THAT IS BEING USED)
+    LanguageLetters.openFile("wordFile")
+   
+main()
