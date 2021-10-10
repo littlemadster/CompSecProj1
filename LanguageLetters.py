d4422cabd7565ef628c61c68b14324272ad0f890
Maddie S <77306767+littlemadster@users.noreply.github.com>
2021-10-10 12:14:41 -0400

    Update LanguageLetters.py

--- LanguageLetters.py
+++ LanguageLetters.py
@@ -6,3 +6,20 @@ LanguageLetters.py
 
 class LanguageLetters:
     
+    # function to remove spaces string
+    def removeSpace(string):
+       return "".join(string.replace("\n",""))
+
+    # function to open and save file without spaces in a new file
+    def openFile():
+        
+        text = open("nuDAim.txt", "r") # opens file to read
+        newText =open("newFile","w") # creates new file to write
+
+        file = removeSpace(text.read()) # removes spaces
+        print(file, file=newText) 
+
+        # closes file
+        newText.close() 
+        text.close()
+        return file    
