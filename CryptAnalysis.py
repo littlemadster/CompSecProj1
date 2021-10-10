9049ad35072eb162b64eb3eb8dbd31c6a406f81b
Maddie S <77306767+littlemadster@users.noreply.github.com>
2021-10-10 13:44:29 -0400

    Update CryptAnalysis.py

--- CryptAnalysis.py
+++ CryptAnalysis.py
@@ -1,37 +1,23 @@
-# CryptAnalysis Class
-#from VigenereEncryption import mainVig
-
-
-#asciiLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '=', '+', '*']
-#asciiProb = [10.15, 1.54, 1.49, 4.70, 9.38, 2.03, 2.86, 2.09, 8.43, 0.61, 3.14, 5.28, 8.54, 3.47, 1.92, 1.84, 0.02, 5.82, 6.59, 7.69, 4.48, 2.42, 0.14, 0.16, 0.71, 0.07, 1.34, 1.80, 1.31]
+"""
+Project 1
+Maddie Sirok & Valentina Colorado
+"""
 
 class CryptAnalysis:
-    
+
     def printProb(string, lettersBase, probBase):
 
         letterCount = [0] * 29
         letterProb = [0] * 29
 
-        for letterString in range(len(string)): # loops through each letter in string
-            for letterAlph in range(29): # loops through each letter in alphabet
-                if lettersBase[letterAlph] == string[letterString]: # checks letter match
-                    # increase count for letter
+        for letterString in range(len(string)): #loops through each letter in string
+            for letterAlph in range(29): #loops through each letter in alphabet
+                if lettersBase[letterAlph] == string[letterString]: 
+                    #increase count for letter
                     letterCount[letterAlph] += 1
-                    
-                    
-        print('Imported Prob:' + '\t' + 'Real Prob:')
-        for j in range(29): # loops through alphabet
-            letterProb[j] = (letterCount[j] / len(string)) * 100 # calculates probability
-            # prints probability of letter in a 'table' comparison to actualy probability
-            print(str(lettersBase[j]) + ': ' + str("{:.4f}".format(letterProb[j])) + "\t" + lettersBase[j] + ': ' + str("{:.4f}".format(asciiProb[j])))
 
+        for j in range(29):
+            letterProb[j] = (letterCount[j] / len(string)) * 100
+            print(str(lettersBase[j]) + ': ' + str("{:.4f}".format(letterProb[j])) + "\t" + lettersBase[j] + ': ' + str("{:.4f}".format(probBase[j])))
+            
 
-"""
-def main():
-    str = mainVig()
-    print("string received!! \n running crypto analysis: ")
-    printProb(str, asciiLetters, asciiProb)
-    
-
-main()
-"""
