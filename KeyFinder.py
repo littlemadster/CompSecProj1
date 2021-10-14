1fd6ae3c382460a58bc4f92ec162565e65b1f1d3
ValColorado <valentina_colorado@yahoo.com>
2021-10-14 11:58:25 -0400

    KeyFinder.py

--- KeyFinder.py
+++ KeyFinder.py
@@ -8,6 +8,7 @@ from LanguageLetters import LanguageLetters
 
 class KeyFinder:
 
+
     def main(fileName,keyLength):
 
 
@@ -16,15 +17,14 @@ class KeyFinder:
         originalSplit = original[0:50] # take only the first 50 char 
         print("\n"+originalSplit)
 
-        for i in range(keyLength): #print sequence and shift i spaces 
-            print("Sequence",i,originalSplit[i:] +originalSplit[:i])
-         
-
-
-        #need to calculate IC
+ #need to calculate IC
+        for i in range (keyLength):
+                print("If key length were: ",i)
+                for j in range (i+1): #print sequence and shift i spaces 
+                    print("Sequence",j,originalSplit[j:] +originalSplit[:j])
+                    
+            
 
         return originalSplit
-
-
-
-
+        
+   
