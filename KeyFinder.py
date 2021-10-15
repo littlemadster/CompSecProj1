fb35e786f77a0f4dc6b94a6a7ec44a37224ba26c
Maddie S <77306767+littlemadster@users.noreply.github.com>
2021-10-15 19:07:26 -0400

    Add files via upload

--- KeyFinder.py
+++ KeyFinder.py
@@ -9,22 +9,92 @@ from LanguageLetters import LanguageLetters
 class KeyFinder:
 
 
-    def main(fileName,keyLength):
+    def findKeyLength(fileName, keyLength): # TODO: EDIT TO BE KEY
 
+        keyLength = keyLength
 
-        #print original string
         original = LanguageLetters.openFile(fileName)
-        originalSplit = original[0:50] # take only the first 50 char 
-        print("\n"+originalSplit)
+        fileName = original[0:50] # take only the first 50 char 
+        print("\n"+fileName + "\n")
+        
+        if keyLength == 1:
+            print("Key Length = 1:")
+                  
+            string1 = ''
+            
+            for i in range(0, keyLength):
+                string1 = fileName
+            print(string1)
 
- #need to calculate IC
-        for i in range (keyLength):
-                print("If key length were: ",i)
-                for j in range (i+1): #print sequence and shift i spaces 
-                    print("Sequence",j,originalSplit[j:] +originalSplit[:j])
-                    
+        if keyLength == 2:
+            print("Key Length = 2:")
+
+            string1 = ''
+            string2 = ''
+            
+            for i in range(0, keyLength, 2):
+                string1 = fileName[i::i+2]
+                string2 = fileName[i+1::i+2]
+                
+            print(string1)
+            print(string2)
+                
+        if keyLength == 3:
+            print("Key Length = 3:")
+
+            string1 = ''
+            string2 = ''
+            string3 = ''
+            
+            for i in range(0, keyLength, 3):
+                string1 = fileName[i::i+3]
+                string2 = fileName[i+1::i+3]
+                string3 = fileName[i+2::i+3]
+                
+            print(string1)
+            print(string2)
+            print(string3)
             
 
-        return originalSplit
+        if keyLength == 4:
+            print("Key Length = 4:")
+
+            string1 = ''
+            string2 = ''
+            string3 = ''
+            string4 = ''
+            
+            for i in range(0, keyLength, 4):
+                string1 = fileName[i::i+4]
+                string2 = fileName[i+1::i+4]
+                string3 = fileName[i+2::i+4]
+                string4 = fileName[i+3::i+4]
+                
+            print(string1)
+            print(string2)
+            print(string3)
+            print(string4)
+
+        if keyLength == 5:
+            print("Key Length = 5:")
+
+            string1 = ''
+            string2 = ''
+            string3 = ''
+            string4 = ''
+            string5 = ''
+            
+            for i in range(0, keyLength, 5):
+                string1 = fileName[i::i+5]
+                string2 = fileName[i+1::i+5]
+                string3 = fileName[i+2::i+5]
+                string4 = fileName[i+3::i+5]
+                string5 = fileName[i+4::i+5]
+                
+            print(string1)
+            print(string2)
+            print(string3)
+            print(string4)
+            print(string5)
         
    
