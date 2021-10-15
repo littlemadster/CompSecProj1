8d20d9d6495774752d04593665c2193cf1cb0403
Maddie S <77306767+littlemadster@users.noreply.github.com>
2021-10-15 17:16:01 -0400

    Add files via upload

--- CryptAnalysis.py
+++ CryptAnalysis.py
@@ -5,19 +5,27 @@ Maddie Sirok & Valentina Colorado
 
 class CryptAnalysis:
 
-    def printProb(string, lettersBase, probBase):
+    def letterCounter(string, alphabet):
 
-        letterCount = [0] * 29
-        letterProb = [0] * 29
+        # creates array for counter for letters and array for probabilities of letters
+        letterCount = [0] * 29    
 
         for letterString in range(len(string)): #loops through each letter in string
             for letterAlph in range(29): #loops through each letter in alphabet
-                if lettersBase[letterAlph] == string[letterString]: 
+                if alphabet[letterAlph] == string[letterString]: 
                     #increase count for letter
                     letterCount[letterAlph] += 1
 
-        for j in range(29):
-            letterProb[j] = (letterCount[j] / len(string)) * 100
-            print(str(lettersBase[j]) + ': ' + str("{:.4f}".format(letterProb[j])) + "\t" + lettersBase[j] + ': ' + str("{:.4f}".format(probBase[j])))
-            
+        return letterCount
 
+
+    def printProb(string, alphabet, probAlphab): # takes string, alphabet, probability table, and size of key (int)
+
+        lettCount = letterCounter(string, alphabet)
+
+        for j in range(29): # loops to calculate and print probabilities
+            letterProb[j] = (lettCount[j] / len(string)) * 100
+            print(str(alphabet[j]) + ': ' + str("{:.4f}".format(letterProb[j])) + "\t" + alphabet[j] + ': ' + str("{:.4f}".format(probAlphab[j])))
+ 
+
+            
