b331bcc24f2bb27c80a8718e0f0e19fd0e4d4abb
Maddie S <77306767+littlemadster@users.noreply.github.com>
2021-10-19 10:10:51 -0400

    Update VigenereEncryption.py

--- VigenereEncryption.py
+++ VigenereEncryption.py
@@ -5,8 +5,7 @@ Encrypt & Decrypt Vigenere Cipher
 import math
 from openText import open_text
 
-#asciiValues[29] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
-asciiLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '=', '+', '*']
+asciiLetters = ['=', '+', '*', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 
 # y = e + k mod 26
