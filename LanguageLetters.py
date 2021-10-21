c7a1cb28248691bc1ac6326b1193047e39df5766
ValColorado <valentina_colorado@yahoo.com>
2021-10-21 10:06:42 -0400

    Final Version LanguageLetters.py

--- LanguageLetters.py
+++ LanguageLetters.py
@@ -2,27 +2,32 @@
 Project 1 LanguageLetters.py 
 Maddie Sirok & Valentina Colorado
 CIS4367.01 
-Last modified 10/20/21 more information on https://github.com/littlemadster/CompSecProj1/tree/main
+Last modified 10/20/21
 
 Class LanguageLetters allows user to open text file, remove white spaces, and save it into a new file. 
 To impliment this class use TESTER-LanguageLetter.py 
 """
+
+from typing import NewType
+import re
 
 class LanguageLetters:
       
   print("Running Language Letters")    
  
   def remove(string): # function to remove spaces string
-    return "".join(string.replace("\n",""))
+    pattern = re.compile(r'\s+') #removes 
+    string = re.sub(pattern,'',string)
+    return string
         
   def openFile(fileName): #opens and saves file without spaces in a new file
-    print("running open text")
+    print("\nrunning LanguageLetters.py ")
     text = open(fileName+".txt", "r")   #open original and new files
     newText =open("newFile","w")        #creates "newFile" 
     file = LanguageLetters.remove(text.read()) #calls remove function
     print(file, file=newText)           #prints contents of file into new file that stores original file minus the white space
-
-    print(file[0:100])############################################
+    
+    print(file[0:300])
     newText.close() #close file
     text.close()    #close file
     
